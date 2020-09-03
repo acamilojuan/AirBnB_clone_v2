5bd7e5d5d6f46d0c07313723a291e39d389f5282
acamilojuan <1600@holbertonschool.com>
2020-09-02 19:08:02 -0500

    Task 4 created and tested

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Method to start the Flask Web app"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Method to start the Flask Web app"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def cfun(text):
+    """Method to start the Flask Web app"""
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def py(text="is cool"):
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return str(n) + " is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
